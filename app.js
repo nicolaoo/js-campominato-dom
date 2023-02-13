f3d509d06ee5a630cba33b9eec0c661f791c465c
nicola00 <nicolajobs00@gmail.com>
2023-02-13 10:46:41 +0100

    Correzzione in classe

--- app.js
+++ app.js
@@ -5,13 +5,21 @@ let numCelle = numGriglie * numGriglie
 
 let grigliaElement = document.querySelector('.griglia')
 let btnElement = document.getElementById('start')
-
+let celleEl
 
 console.log(numCelle, grigliaElement)
 
-btnElement.addEventListener('click', function ( ) {
+let bombe = []
+
+punteggio = 0
+
+btnElement.addEventListener('click', function() {
     //reset griglia
     grigliaElement.innerHTML = ' '
+
+    punteggio.innerHTML = ' '
+
+    bombe = generaBombe(16,1,numCelle)
 
     for (let i = 1; i <= numCelle; i++){
     numCorrente = i
@@ -22,29 +30,100 @@ btnElement.addEventListener('click', function ( ) {
     }
     
     //generare un numeo random
-    let numCasuali
-    for ( let i = 0; i < 16; i++){
-        let numCasuali = Math.floor(Math.random() * (numCelle - 1 + 1) + 1)
-        console.log(numCasuali)
-    }
-
     
+    
+    celleEl = document.querySelectorAll('.cella')
 
-    let celleEl = document.querySelectorAll('.cella')
-
-    for (let i = 0; i < celleEl.length; i++) {
+    for (let i = 0; i < celleEl.length; i++){
         const cell = celleEl[i]
         
-        cell.addEventListener('click', function () {
-            console.log(cell)
-            cell.classList.add('color')
-            
-        })
-
+        cell.addEventListener('click', onclick)
+       
     }
 
+
+    // for ( let i = 0; i < 16; i++){
+    //     
+        
+    //     let numArray = bombe.push(numCasuali)
+        
+       
+    //     // se lutente clicca su uno dei numeri delle bombe faccio comparire la bomba e resetto
+
+    //     // console.log(numArray, bombe, num)
+
+    // }
 
 
 })
 
 
+function onclick(click){
+    let cella = this
+    console.log(cella.innerHTML)
+
+    let numCella = parseInt(cella.innerHTML)
+
+    if (isBomb(numCella)) {
+        console.log('hai perso')
+        gameOver()
+    } else {
+        console.log('hai vinto')
+        punteggio++
+        // numCella.class.add('bg-green')
+
+
+        // controllo se l'utente ha vinto cliccando tutte le bombe
+        let vittoria = numCelle - bombe.length
+        if (vittoria === punteggio){
+            console.log('hai vinto')
+
+        }
+    }
+
+    cella.removeEventListener('click', onclick)
+}
+
+function isBomb (numero) {
+    let result = bombe.includes(numero)  // potevamo metterlo nell'if direttamente 
+
+    return result
+}
+
+function generaBombe (numeroBomb,min,max){
+    let arrayBombe=[]
+
+    while (arrayBombe.length < numeroBomb) {
+        let numCasuali = getRandomIntInclusive(min,max)
+
+        if (arrayBombe.includes(numCasuali) === false){
+            arrayBombe.push(numCasuali)
+        }
+        
+    }
+
+
+    return arrayBombe
+}
+
+function gameOver( ){
+    console.log('hai perso')
+
+    for(let i = 0; i < celleEl.length; i++) {
+        let cella = celleEl[i]
+        
+        let numeroCella = parseInt( cella.innerHTML)
+
+        if ( isBomb(numCelle)) {
+            // cella.classList.add('bg-red')
+        }
+
+        cella.removeEventListener('click', onclick)
+    }
+}
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+  }
